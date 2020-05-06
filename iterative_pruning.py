9cc0516b8f4ce717c5c0921ebbf76d31038e37b1
Jos3f <Jos3f@users.noreply.github.com>
2020-05-06 16:43:49 +0200

    Bug fix

--- iterative_pruning.py
+++ iterative_pruning.py
@@ -1,5 +1,8 @@
 import numpy as np
 import copy
+import tensorflow as tf
+from Lenet import Lenet
+
 
 
 class Model:
@@ -76,26 +79,29 @@ class IterativeTrainer:
         self.iterations = iterations
 
 
-    def train_iter(self, iterations):
+    def train_iter(self, iterations, x_train, y_train, x_test,  y_test):
         """
         Train iteratively
         """
         # Train full model once first
-        self.model.train()
+        # self.model.train()
+        self.model.train(x_train, y_train, x_test, y_test)
 
         # Initial masks are set to 1. This corresponds to not including all weights
         masks = {}
         layers = self.model.get_trainable_weights()
-        for key, weights in layers:
+        for key, weights in layers.items():
             masks[key] = np.ones_like(weights)
 
         # Start the iterative pruning process
         for it in range(iterations):
+            print("Iteration {}".format(it + 1))
             # Update masks by pruning
             masks = self.prune(masks, layers)
             # Train with our new masking
             self.model.reset()
-            self.model.train(masks)
+            # self.model.train()
+            self.model.train(x_train, y_train, x_test,  y_test, masks)
             # Get the new updated layer weights
             layers = self.model.get_trainable_weights()
 
@@ -110,7 +116,7 @@ class IterativeTrainer:
             # Calculate how many weights we need to prune
             active_nodes = (masks[layer_name] == 1)  # indices of active nodes
             prune_percentages = self.model.get_prune_percentages()
-            prune_count = int(round(prune_percentages[layer_name] * active_nodes.size))
+            prune_count = int(round(prune_percentages[layer_name] * np.sum(active_nodes)))
 
             # Sort by the magnitude of the weights for the weights in our mask and extract the element value where
             # we have gone past 'prune_count' nodes.
@@ -120,10 +126,26 @@ class IterativeTrainer:
             magnitude_under_cutoff = (np.absolute(layers[layer_name]) <= cutoff_value)
             updated_masks[layer_name][magnitude_under_cutoff] = 0
 
-            return updated_masks
+        return updated_masks
+
+    def update_percentages(self, iteration, percentage_each_it):
+        new_percentage = 1 - (1 - percentage_each_it)**iteration
+        return new_percentage
 
 
 def main():
+    """Begin with lenet"""
+    mnist = tf.keras.datasets.mnist
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    print(x_train.shape)
+
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+    prune_percentages = {'layer0': .2, 'layer1': .2, 'layer2': .2, 'layer3': .2, 'layer4': .1, 'layer5': .1}
+    lenet_net = Lenet(prune_percentages)
+    # lenet_net.train(x_train, y_train, x_test,  y_test)
+
+    iter_trainer = IterativeTrainer(lenet_net)
+    iter_trainer.train_iter(5, x_train, y_train, x_test,  y_test)
     # 1: Create model
     # 2: Create IterativeTrainer with model
     # 3: Call IterativeTrainer.train_iter()
